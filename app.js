ffaa4486b59d9fdf3569851beb37750974c37d53
dvthnight <davide.lanotte@protonmail.com>
2022-02-17 17:57:50 +0100

    esercizio completato

--- app.js
+++ app.js
@@ -3,32 +3,32 @@ const team = [
     {
       name: "Wayne Barnett",
       role: "Founder & CEO",
-      image: "wayne-barnett-founder-ceo.jpg",
+      image: "img/wayne-barnett-founder-ceo.jpg",
     },
     {
       name: "Angela Caroll",
       role: "Chief Editor",
-      image: "angela-caroll-chief-editor.jpg",
+      image: "img/angela-caroll-chief-editor.jpg",
     },
     {
       name: "Walter Gordon",
       role: "Office Manager",
-      image: "walter-gordon-office-manager.jpg",
+      image: "img/walter-gordon-office-manager.jpg",
     },
     {
       name: "Angela Lopez",
       role: "Social Media Manager",
-      image: "angela-lopez-social-media-manager.jpg",
+      image: "img/angela-lopez-social-media-manager.jpg",
     },
     {
       name: "Scott Estrada",
       role: "Developer",
-      image: "scott-estrada-developer.jpg",
+      image: "img/scott-estrada-developer.jpg",
     },
     {
       name: "Barbara Ramos",
       role: "Graphic Designer",
-      image: "barbara-ramos-graphic-designer.jpg",
+      image: "img/barbara-ramos-graphic-designer.jpg",
     },
   ];
 
@@ -39,8 +39,8 @@ const team = [
     const cardTeamMember = `<div class="team-card">
                             <div class="card-image">
                                 <img
-                                src=${immagine}
-                                alt=${nome}
+                                src="${immagine}"
+                                alt="${nome}"
                                 />
                             </div>
                             <div class="card-text">
@@ -53,6 +53,10 @@ const team = [
 
   const teamContainer  = document.querySelector(".team-container");
 
+//   ciclo for che mi prende i dati dei membri
+//   li inoltra alla funzione che restituisce il codice html 
+//   questo codice viene poi inserito nell'HTML
+
   for(let i=0; i< team.length; i++){
       const nomeMembro = team[i].name;
       const imggMembro = team[i].image;
@@ -61,4 +65,6 @@ const team = [
 
       const cardMember = generatoreCard(nomeMembro,ruoloMembro,imggMembro);
       console.log(cardMember);
+
+      teamContainer.innerHTML += cardMember;
   }
